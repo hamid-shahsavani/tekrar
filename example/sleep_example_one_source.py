a004e0e8e84bd6314ba02ac2a7152ac018c72231
sys113 <24222077+sys113@users.noreply.github.com>
2019-12-08 13:04:04 -0800

    upload sleep example one

---
+++ example/sleep_example_one_source.py
@@ -0,0 +1,12 @@
+# arguments used : sleep , speed , method
+
+# arguments default value , speed = 3 , method = 1
+
+from tekrar import loading
+from os import system
+
+print('tekrar ...')
+
+loading(sleep = 10 , speed = 6 , method = 2)
+
+system('figlet \'tekrar\' | lolcat')
