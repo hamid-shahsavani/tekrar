bd983851be46025e82f687522124b3073f29f9f7
sys113 <24222077+sys113@users.noreply.github.com>
2019-12-09 23:48:12 +0330

    change version 0.1.0 from 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
   name = "tekrar",
-  version = "0.1.0",
+  version = "0.1.1",
   description = "create loading animation in python cli project ...",
   long_description=long_description,
   long_description_content_type="text/markdown",
